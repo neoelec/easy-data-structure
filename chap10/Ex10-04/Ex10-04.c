31edbca86338848f642118a747394b0ce9c2efae
YOUNGJIN JOO <neoelec@gmail.com>
2024-09-19 00:43:26 +0900

    (easy-data-structure): chap10/Ex10-04: updated
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- chap10/Ex10-04/Ex10-04.c
+++ chap10/Ex10-04/Ex10-04.c
@@ -13,16 +13,12 @@ static int INT_Compare(const void *_a, const void *_b)
 int main(int argc, char *argv[])
 {
     int data[8] = { 69, 10, 30, 2, 16, 8, 31, 22 };
-    void *ptr_arr[NR_ELEM(data)];
     size_t i;
 
-    for (i = 0; i < NR_ELEM(data); i++)
-        ptr_arr[i] = &data[i];
-
-    BubbleSort(ptr_arr, NR_ELEM(data), INT_Compare);
+    BubbleSort(data, NR_ELEM(data), sizeof(data[0]), INT_Compare);
 
     for (i = 0; i < NR_ELEM(data); i++)
-        printf("%d ", *(int *)ptr_arr[i]);
+        printf("%d ", data[i]);
 
     printf("\n");
 
