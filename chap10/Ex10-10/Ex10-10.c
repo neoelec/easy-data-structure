d2bd0e74aa8a85944cbe9e5a234fd23601202657
YOUNGJIN JOO <neoelec@gmail.com>
2024-09-19 03:16:50 +0900

    (easy-data-structure): chap10/Ex10-10: updated
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- chap10/Ex10-10/Ex10-10.c
+++ chap10/Ex10-10/Ex10-10.c
@@ -4,13 +4,13 @@
 
 int main(int argc, char *argv[])
 {
-    unsigned long data[8] = { 69, 10, 30, 2, 16, 8, 31, 22 };
+    int data[8] = { 69, 10, 30, 2, 16, 8, 31, 22 };
     size_t i;
 
-    RadixSort(data, NR_ELEM(data), 10);
+    RadixSort(data, NR_ELEM(data), sizeof(data[0]), 3);
 
     for (i = 0; i < NR_ELEM(data); i++)
-        printf("%zu ", data[i]);
+        printf("%d ", data[i]);
 
     printf("\n");
 
