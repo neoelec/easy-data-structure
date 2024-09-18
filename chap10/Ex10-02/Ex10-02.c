ff652c4f046752e5f5e5e1c61addb98984264d87
YOUNGJIN JOO <neoelec@gmail.com>
2024-09-19 00:43:22 +0900

    (easy-data-structure): chap10/Ex10-02: updated
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- chap10/Ex10-02/Ex10-02.c
+++ chap10/Ex10-02/Ex10-02.c
@@ -13,16 +13,13 @@ static int INT_Compare(const void *_a, const void *_b)
 int main(int argc, char *argv[])
 {
     int data[8] = { 69, 10, 30, 2, 16, 8, 31, 22 }; // 정렬할 초기 원소 배열
-    void *ptr_arr[NR_ELEM(data)];
     size_t i;
 
-    for (i = 0; i < NR_ELEM(data); i++)
-        ptr_arr[i] = &data[i];
-
-    SelectionSort(ptr_arr, NR_ELEM(data), INT_Compare); // 선택 정렬 연산 호출
+    SelectionSort(data, NR_ELEM(data), sizeof(data[0]),
+        INT_Compare); // 선택 정렬 연산 호출
 
     for (i = 0; i < NR_ELEM(data); i++)
-        printf("%d ", *(int *)ptr_arr[i]);
+        printf("%d ", data[i]);
 
     printf("\n");
 
